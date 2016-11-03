5abc29cd16ae29d363b83eaf00adfa5df3bfb373
Mike J H <matrixmike@icloud.com>
2016-11-04 00:46:32 +1100

    cosmetic change to allow push

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@
 #include "class1.cpp"
 //int factorial(int n);
 
- int main(){
+int main()
+{
 //   print_hello();
 //    std::cout << endl;
-    std::cout << "The factorial of 5 is "; // << factorial(5) << "\n";
-    std::cout << "\n";
-    
-    stack stack;
-    return 0;
+	std::cout << "The factorial of 5 is ";	// << factorial(5) << "\n";
+	std::cout << "\n";
+
+	stack stack;
+	return 0;
 }
