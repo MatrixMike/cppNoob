e157c96eec5d9f6ad631ca3bbec13748064b74c8
Mike Hewitt <matroxmike@icloud.com>
2017-01-21 18:46:04 +1100

    cout trial

--- mainA.cpp
+++ mainA.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 const int SIZE = 10;
 
-class safearay {
+class safearray {
  private:
 	int arr[SIZE];
  public:
-	 safearay()		// constructor that initialises storage
+	 safearray()		// constructor that initialises storage
 	{
 		register int i;
 		for (i = 0; i < SIZE; i++) {
@@ -24,11 +24,13 @@ class safearay {
 
 int main()
 {
-	safearay A;
+	safearray A;
 
 	cout << "Value of A[ 2] : " << A[2] << endl;
 	cout << "Value of A[ 5] : " << A[5] << endl;
 	cout << "Value of A[12] : " << A[12] << endl;
+// same effect as above in one line...
+	cout << "Value of A[ 2] : " << A[2] << endl << "Value of A[ 5] : " << A[5] << endl << "Value of A[12] : " << A[12] << endl;
 
 	return 0;
 }
