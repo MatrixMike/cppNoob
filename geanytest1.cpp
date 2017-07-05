19a9db07c4db186b51c2cffc348686b4014e686e
Mike J Hewitt <mikeh@electroteach.com>
2017-07-05 13:39:14 +1000

    geany plugin test

---
+++ geanytest1.cpp
@@ -0,0 +1,33 @@
+/*
+ * geanytest1.cpp
+ * 
+ * Copyright 2017 mikeh <mikeh@mikeh-desktop>
+ * 
+ *  * class created from geany plugin 
+ * 
+ */
+
+
+#include "geanytest1.hpp"
+
+
+geanytest1::geanytest1()
+{
+	
+}
+
+
+geanytest1::~geanytest1()
+{
+	
+}
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
---
+++ geanytest1.hpp
@@ -0,0 +1,23 @@
+/*
+ * geanytest1.hpp
+ * 
+ * Copyright 2017 mikeh <mikeh@mikeh-desktop>
+ * 
+ * class created from geany plugin 
+ */
+
+
+#ifndef GEANYTEST1_HPP
+#define GEANYTEST1_HPP
+
+class geanytest1
+{
+	public:
+		geanytest1();
+		virtual ~geanytest1();
+	
+	private:
+		/* add your private declarations */
+};
+
+#endif /* GEANYTEST1_HPP */ 
