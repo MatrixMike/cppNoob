d37a8f78ea936703bf5bedc1246f1d383c4101d8
Mike J Hewitt <mikeh@electroteach.com>
2017-12-20 12:51:26 +1100

    added some debug

--- gcf.cpp
+++ gcf.cpp
@@ -24,24 +24,30 @@
 #include <iostream>
 using namespace std;
 
-int hcf(int n1, int n2);
+int hcf (int n1, int n2);
 
-int main()
+int
+main ()
 {
-   int n1, n2;
+  int n1, n2;
 
-   cout << "Enter two positive integers: ";
-   cin >> n1 >> n2;
+  cout << "Enter two positive integers: ";
+  cin >> n1 >> n2;
 
-   cout << "H.C.F of " << n1 << " & " <<  n2 << " is: " << hcf(n1, n2);
+  cout << "H.C.F of " << n1 << " & " << n2 
+		<< " is: " << hcf (n1, n2) << endl ;
 
-   return 0;
+  return 0;
 }
 
-int hcf(int n1, int n2)
+int
+hcf (int n1, int n2)
 {
-    if (n2 != 0)
-       return hcf(n2, n1 % n2);
-    else 
-       return n1;
+  if (n2 != 0)
+    {
+      cout << "debug : " << n2 << " : " << n1 << endl;
+      return hcf (n2, n1 % n2);
+    }
+  else
+    return n1;
 }
